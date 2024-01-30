fc5d7ada21664873914ff8b717e8a7e511b6cd47
CaioNunesS <caiogrb@gmail.com>
2024-01-29 22:12:03 -0300

    first commit

---
+++ ReactJs/01-fundamentos-do-reactjs/src/App.jsx
@@ -0,0 +1,19 @@
+import {Post} from "./Post"
+
+export function App() {
+
+  return (
+    <div>
+      <Post
+        author="Caio Nunes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, porro?"
+      />
+      <Post
+        author="Caio NunesS"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, porro?"
+      />
+
+    </div>
+  )
+}
+
